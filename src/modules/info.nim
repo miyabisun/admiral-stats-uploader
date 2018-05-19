c772a84c73c235a852c55256feeddc338dd3e2cc
miyabisun <miyabi.ooh@gmail.com>
2018-05-19 12:37:23 +0900

    delete comments in modules

--- src/modules/info.nim
+++ src/modules/info.nim
@@ -1,19 +1,5 @@
 import httpclient, strutils
 
-#[
-curl 'https://kancolle-arcade.net/ac/api/Personal/basicInfo'
--H 'Pragma: no-cache'
--H 'Accept-Encoding: gzip, deflate, br'
--H 'Accept-Language: ja,en-US;q=0.9,en;q=0.8'
--H 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.117 Safari/537.36'
--H 'Accept: application/json, text/plain, */*'
--H 'Referer: https://kancolle-arcade.net/ac/'
--H 'X-Requested-With: XMLHttpRequest'
--H 'Cookie: JSESSIONID=0000X4XKCF_FFDSFmhrzo1r_J3D:19jfb2id6'
--H 'Connection: keep-alive'
--H 'Cache-Control: no-cache' --compressed
-#]#
-
 proc info*(api_url: string, cookie: string): string =
   let url = "https://kancolle-arcade.net/ac/api/" & api_url
   echo url
--- src/modules/login.nim
+++ src/modules/login.nim
@@ -1,21 +1,5 @@
 import httpclient, json, strutils, sequtils, tables
 
-#[
-curl 'https://kancolle-arcade.net/ac/api/Auth/login'
--H 'Pragma: no-cache'
--H 'Origin: https://kancolle-arcade.net'
--H 'Accept-Encoding: gzip, deflate, br'
--H 'Accept-Language: ja,en-US;q=0.9,en;q=0.8'
--H 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.117 Safari/537.36'
--H 'Content-Type: application/json;charset=UTF-8'
--H 'Accept: application/json, text/plain, */*'
--H 'Cache-Control: no-cache'
--H 'X-Requested-With: XMLHttpRequest'
--H 'Cookie: JSESSIONID=00006WArNTOpyYMRQxfc8RfVwxU:19jfb2id6'
--H 'Connection: keep-alive'
--H 'Referer: https://kancolle-arcade.net/ac/' --data-binary '{"id":"miyabisun","password":"miyabi00"}' --compressed
-]#
-
 proc preLogin (client: HttpClient): bool =
   const url = "https://kancolle-arcade.net/ac/"
   client.headers = newHttpHeaders({
