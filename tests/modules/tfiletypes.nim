bc321df22bf8635ffe789a8bfaffa37582745156
miyabisun <miyabi.ooh@gmail.com>
2018-05-20 04:54:24 +0900

    add test in filetypes

--- tests/modules/tfiletypes.nim
+++ tests/modules/tfiletypes.nim
@@ -2,6 +2,9 @@ import unittest, ospaths, json, ../../src/modules/filetypes
 let token = ospaths.get_env "AS_TOKEN"
 
 suite "modules/filetypes":
+  test "is failed":
+    let types = filetypes("")
+    check(types.len == 0)
   test "is successful":
     let types = filetypes(token)
     check(types.len > 0)
