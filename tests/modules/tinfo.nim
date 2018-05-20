f0b6e47974e4676fdcf40383693954506ef0a2c2
miyabisun <miyabi.ooh@gmail.com>
2018-05-20 14:42:05 +0900

    update test info module, all filetypes

--- tests/modules/tinfo.nim
+++ tests/modules/tinfo.nim
@@ -1,11 +1,14 @@
-import unittest, ospaths, ../../src/modules/info, ../../src/modules/login
+import unittest, ospaths, strutils, sequtils, ../../src/modules/info, ../../src/modules/login, ../../src/modules/filetypes
 let id = ospaths.get_env "AS_ID"
 let pass = ospaths.get_env "AS_PASS"
+let token = ospaths.get_env "AS_TOKEN"
 
 suite "modules/info":
   let cookie = login(id, pass)
-  test "successful":
-    var result = info("Personal/basicInfo", cookie)
-    echo result
-    check(result != "")
+  let types = filetypes(token)
+  # @["Personal_basicInfo", "TcBook_info", "CharacterList_info", "Event_info", "BlueprintList_info", "EquipBook_info", "EquipList_info"]
+  for path in types:
+    test path & ": successful":
+      var result = info(path.split("_").join("/"), cookie)
+      check(result != "")
 
