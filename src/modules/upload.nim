f0dc07ff0367ba4442666fbf578d371656079be1
miyabisun <miyabi.ooh@gmail.com>
2018-05-20 14:58:41 +0900

    update upload test, all filetypes

--- src/modules/upload.nim
+++ src/modules/upload.nim
@@ -1,14 +1,15 @@
-import times, os, httpclient, strutils, sequtils
+import os, httpclient
 
-proc upload* (token: string, file_type: string, body: string): bool =
-  let time = format(now(), "yyyyMMdd,HHmmss").split(",").join("_")
+proc upload* (token: string, file_type: string, body: string, time: string): bool =
   let url = "https://www.admiral-stats.com/api/v1/import/" & file_type & "/" & time
+  echo url
   let client = newHttpClient()
   client.headers = newHttpHeaders({
+    "Content-Type": "application/json",
     "User-Agent": "AdmiralStatsExporter-Nim-CLI",
     "Authorization": "Bearer " & token,
   })
   let response = client.request(url, HttpPost, body)
   echo file_type & "(" & response.status & "): " & response.body
-  result = response.status == "201 Created"
+  result = response.status == "201 Created" or response.status == "200 OK"
 
--- tests/modules/tupload.nim
+++ tests/modules/tupload.nim
@@ -1,12 +1,14 @@
-import unittest, ospaths, ../../src/modules/upload, ../../src/modules/filetypes
+import unittest, times, strutils, sequtils, ospaths, ../../src/modules/upload, ../../src/modules/filetypes
 let token = ospaths.get_env "AS_TOKEN"
-const filetype = "Personal_basicInfo"
+let time = format(now(), "yyyyMMdd,HHmmss").split(",").join("_")
 
 suite "modules/upload":
-  test "is failed":
-    check(upload(token, filetype, "test") == false)
-  test "is successful":
-    let file = open "./tests/data/" & filetype & ".json"
-    let entity = file.readAll()
-    check(upload(token, filetype, entity) == true)
+  let types = filetypes(token)
+  for path in types:
+    test path & ": is failed":
+      check(upload(token, path, "", time) == false)
+    test path & ": is successful":
+      let file = open "./tests/data/" & path & ".json"
+      let entity = file.readAll()
+      check(upload(token, path, entity, time) == true)
 
