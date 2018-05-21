afd2fbde899725a17b6fcee01a8ba9f8b7063a42
miyabisun <miyabi.ooh@gmail.com>
2018-05-22 02:34:03 +0900

    add client and verbose arguments

--- src/modules/update.nim
+++ src/modules/update.nim
@@ -1,18 +1,18 @@
 from parseoptions import asuOptions
 import times, strutils, sequtils, filetypes, login, info, upload
 
-proc update*(options: asuOptions): bool =
+proc update*(client: HttpClient, options: asuOptions): bool =
   let time = format(now(), "yyyyMMdd,HHmmss").split(",").join("_")
 
   # タイプ一覧を取得
-  let types = filetypes(options.token)
+  let types = client.filetypes(options.token, options.verbose)
   if (types.len == 0):
     echo "Admiral Statusにアクセスできませんでした。"
     echo "APIトークンが正しい事を確認してください。"
     return false
 
   # 提督情報にログイン
-  let cookie = login(options.id, options.pass)
+  let cookie = client.login(options.id, options.pass, options.verbose)
   if (cookie == ""):
     echo "提督情報にログイン出来ませんでした。"
     echo "SEGA IDやPasswordが正しい事を確認してください。"
@@ -21,11 +21,11 @@ proc update*(options: asuOptions): bool =
   # 提督情報をダウンロードし、Admiral Statsにアップロード
   for path in types:
     echo path & "の更新..."
-    var json = info(path.split("_").join("/"), cookie)
+    var json = client.info(path.split("_").join("/"), cookie, options.verbose)
     if (json == ""):
       echo "提督情報のJSONのダウンロードに失敗しました。"
       continue
-    var pathResult = upload(options.token, path, json, time)
+    var pathResult = client.upload(options.token, path, json, time, options.verbose)
     if (pathResult == false):
       echo "Admiral Statsのアップロードに失敗しました。"
       continue
--- tests/modules/tupdate.nim
+++ tests/modules/tupdate.nim
@@ -1,10 +1,13 @@
-import unittest, ospaths, ../../src/modules/update, ../../src/modules/parseoptions
+import unittest, ospaths, httpclient, ../../src/modules/update, ../../src/modules/parseoptions
 
 suite "modules/update":
   let id = ospaths.get_env "AS_ID"
   let pass = ospaths.get_env "AS_PASS"
   let token = ospaths.get_env "AS_TOKEN"
+  let client = newHttpClient()
+  teardown:
+    client.close
   test "is successful":
     var options = parseoptions("-i:" & id & " -p:" & pass & " -t:" & token)
-    check(update(options) == true)
+    check(client.update(options) == true)
 
