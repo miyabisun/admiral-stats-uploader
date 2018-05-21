cb3081a02d86ed15f8ca858e4eb624982c7bf816
miyabisun <miyabi.ooh@gmail.com>
2018-05-22 02:25:22 +0900

    add client and verbose arguments

--- src/modules/filetypes.nim
+++ src/modules/filetypes.nim
@@ -1,12 +1,13 @@
 import times, os, httpclient, json, sequtils
+export httpclient.HttpClient
 
-proc filetypes* (token: string): seq =
+proc filetypes*(client: HttpClient, token: string, verbose: bool = false): seq =
   const url = "https://www.admiral-stats.com/api/v1/import/file_types"
-  let client = newHttpClient()
   client.headers = newHttpHeaders({
     "User-Agent": "AdmiralStatsExporter-Nim-CLI",
     "Authorization": "Bearer " & token,
   })
   let response = client.request(url, HttpGet)
+  if verbose: echo "file_types(" & response.status & "): " & response.body
   result = response.body.parseJson.getElems.map(proc (it: JsonNode): string = it.getStr)
 
--- tests/modules/tfiletypes.nim
+++ tests/modules/tfiletypes.nim
@@ -1,12 +1,15 @@
-import unittest, ospaths, json, ../../src/modules/filetypes
+import unittest, ospaths, json, httpClient, ../../src/modules/filetypes
 
 suite "modules/filetypes":
   let token = ospaths.get_env "AS_TOKEN"
+  let client = newHttpClient()
+  teardown:
+    client.close
   test "is failed":
-    let types = filetypes("")
+    let types = client.filetypes ""
     check(types.len == 0)
   test "is successful":
-    let types = filetypes(token)
+    let types = client.filetypes token
     check(types.len > 0)
     check(types[0] == "Personal_basicInfo")
 
