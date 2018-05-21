81577ed544e1e8c076a497f56e8e7f71bec96358
miyabisun <miyabi.ooh@gmail.com>
2018-05-22 02:29:54 +0900

    add client and verbose arguments

--- src/modules/login.nim
+++ src/modules/login.nim
@@ -1,17 +1,18 @@
 import httpclient, json, strutils, sequtils, tables
+export httpclient.HttpClient
 
-proc preLogin (client: HttpClient): bool =
+proc preLogin (client: HttpClient, verbose: bool = false): bool =
   const url = "https://kancolle-arcade.net/ac/"
   client.headers = newHttpHeaders({
     "User-Agent": "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.117 Safari/537.36",
   })
   let response = client.request(url, HttpGet)
+  if verbose: echo "[login]prelogin(" & response.status & ")"
   result = response.status == "200 OK"
 
-proc login* (id: string, pass: string): string =
-  let client = newHttpClient()
-  echo "top-page: " & $preLogin client
+proc login*(client: HttpClient, id: string, pass: string, verbose: bool = false): string =
   const url = "https://kancolle-arcade.net/ac/api/Auth/login"
+  discard client.preLogin verbose
   client.headers = newHttpHeaders({
     "Content-Type": "application/json;charset=UTF-8",
     "Referer": "https://kancolle-arcade.net/ac",
@@ -20,10 +21,8 @@ proc login* (id: string, pass: string): string =
   })
   let body = %*{"id": id, "password": pass}
   let response = client.request(url, HttpPost, $body)
-  echo "login-status: ", response.status
+  if verbose: echo "[login]login(" & response.status & ")"
   if response.status == "200 OK":
     let cookies = response.headers.table["set-cookie"]
     result = cookies[cookies.len - 1].split(";")[0]
-  else:
-    result =  ""
 
--- tests/modules/tlogin.nim
+++ tests/modules/tlogin.nim
@@ -1,9 +1,12 @@
-import unittest, ospaths, ../../src/modules/login
+import unittest, ospaths, httpclient, ../../src/modules/login
 
 suite "modules/login":
   let id = ospaths.get_env "AS_ID"
   let pass = ospaths.get_env "AS_PASS"
-  let login_cookie = login(id, pass)
+  let client = newHttpClient()
+  let login_cookie = client.login(id, pass)
+  teardown:
+    client.close
   test "is successful":
     check(login_cookie != "")
 
