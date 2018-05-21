0b405c11f29d87e040291911cbc22e327dcbfe5a
miyabisun <miyabi.ooh@gmail.com>
2018-05-22 02:32:20 +0900

    add client and verbose arguments

--- src/modules/upload.nim
+++ src/modules/upload.nim
@@ -1,15 +1,14 @@
 import os, httpclient
+export httpclient.HttpClient
 
-proc upload* (token: string, file_type: string, body: string, time: string): bool =
+proc upload*(client: HttpClient, token: string, file_type: string, body: string, time: string, verbose: bool = false): bool =
   let url = "https://www.admiral-stats.com/api/v1/import/" & file_type & "/" & time
-  echo url
-  let client = newHttpClient()
   client.headers = newHttpHeaders({
     "Content-Type": "application/json",
     "User-Agent": "AdmiralStatsExporter-Nim-CLI",
     "Authorization": "Bearer " & token,
   })
   let response = client.request(url, HttpPost, body)
-  echo file_type & "(" & response.status & "): " & response.body
+  if verbose: echo "[post]" & file_type & "(" & response.status & "): " & response.body
   result = response.status == "201 Created" or response.status == "200 OK"
 
--- tests/modules/tupload.nim
+++ tests/modules/tupload.nim
@@ -1,14 +1,17 @@
-import unittest, times, strutils, sequtils, ospaths, ../../src/modules/upload, ../../src/modules/filetypes
+import unittest, times, strutils, sequtils, ospaths, httpClient, ../../src/modules/upload, ../../src/modules/filetypes
 
 suite "modules/upload":
   let token = ospaths.get_env "AS_TOKEN"
   let time = format(now(), "yyyyMMdd,HHmmss").split(",").join("_")
-  let types = filetypes(token)
+  let client = newHttpClient()
+  let types = client.filetypes token
+  teardown:
+    client.close
   for path in types:
     test path & ": is failed":
-      check(upload(token, path, "", time) == false)
+      check(client.upload(token, path, "", time) == false)
     test path & ": is successful":
       let file = open "./tests/data/" & path & ".json"
       let entity = file.readAll()
-      check(upload(token, path, entity, time) == true)
+      check(client.upload(token, path, entity, time) == true)
 
